7dec625f96a1204f4dc1aeddd584c8fda4b590b3
Jordan Melendez <jordan.melendez@finalsite.com>
2017-05-25 15:05:36 -0400

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,19 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
 
-var token = jwt.sign(data, '123');
-
-console.log(token);
-
-var decoded = jwt.verify(token, '123');
-
-console.log('decoded', decoded);
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123');
+//
+// console.log('decoded', decoded);
 
 // var message = 'I am user #3';
 // var hash = SHA256(message).toString();
@@ -38,3 +40,21 @@ console.log('decoded', decoded);
 // } else {
 //   console.log('Data was altered, do not trust');
 // }
+
+// Using B Crypt
+// ==================
+
+var password = '123abc!';
+
+//gen salt & hash
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$4ZRVI3k9Y3j1gYsBGo3m6.N5LTVguNp4NnInsTkp.EH6j.dzdWvhi';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
