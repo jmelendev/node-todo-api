77ddd97f1853a5121a33652d46f511ab7e41e4f5
Jordan Melendez <jordan.melendez@finalsite.com>
2017-05-25 23:04:12 -0400

    add POST/users/login route

--- server/server.js
+++ server/server.js
@@ -131,6 +131,20 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+//POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
 //listen for express app
 app.listen(port, () => {
   console.log(`Started up at port ${port}.`);
